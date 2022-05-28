08dc0dfbf96eb96d284139fb49d242f61c48b618
Yoo Hyun Sik <dbgustlr92@gmail.com>
2022-05-28 19:43:57 +0900

    feat: DateExtensions public화

--- Base/extensions/DateExtensions.swift
+++ Base/extensions/DateExtensions.swift
@@ -1,11 +1,11 @@
 import Foundation
 
 extension Date {
-    func addWeek(week: Int) -> Date {
+    public func addWeek(week: Int) -> Date {
         return Calendar.current.date(byAdding: .weekOfYear, value: week, to: self) ?? Date()
     }
     
-    func addMonth(month: Int) -> Date {
+    public func addMonth(month: Int) -> Date {
         return Calendar.current.date(byAdding: .month, value: month, to: self) ?? Date()
     }
 }
